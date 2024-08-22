25456d748d4329d11f1d4c1a137e0940f114069f
Justin Gardner <justin@rhynorater.com>
2024-08-22 06:03:31 -0500

    12.3.21

--- squirrly-seo/squirrly.php
+++ squirrly-seo/squirrly.php
@@ -1,4 +1,4 @@
-<?php defined('ABSPATH') || die('Cheatin\' uh?'); ?>
+<?php defined( 'ABSPATH' ) || die( 'Cheatin\' uh?' ); ?>
 <?php
 /*
  * Copyright (c) 2012-2024, Squirrly.
@@ -9,45 +9,51 @@
  * Description: AI Private SEO Consultant that Brings You the Full Force of SEO: All Schema Rich Results, Inner Links, Redirects, Keyword Research, Real-Time SEO Content, Traffic and SEO Audits, SERP Checker.
  * Author: Squirrly
  * Author URI: https://plugin.squirrly.co
- * Version: 12.3.20
+ * Version: 12.3.21
  * License: GPLv2 or later
  * License URI: http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  * Text Domain: squirrly-seo
  * Domain Path: /languages
  */
 
-if (!defined('SQ_VERSION')) {
-    /* SET THE CURRENT VERSION ABOVE AND BELOW */
-    define('SQ_VERSION', '12.3.20');
-    //The last stable version
-    define('SQ_STABLE_VERSION', '12.3.19');
-    // Call config files
-    try {
-        include_once dirname(__FILE__) . '/config/config.php';
-        include_once dirname(__FILE__) . '/debug/index.php';
+if ( ! defined( 'SQ_VERSION' ) ) {
+	/* SET THE CURRENT VERSION ABOVE AND BELOW */
+	define( 'SQ_VERSION', '12.3.21' );
+	//The last stable version
+	define( 'SQ_STABLE_VERSION', '12.3.20' );
+	// Call config files
+	try {
+		include_once dirname( __FILE__ ) . '/config/config.php';
+		include_once dirname( __FILE__ ) . '/debug/index.php';
 
-        /* important to check the PHP version */
-        // inport main classes
-        include_once _SQ_CLASSES_DIR_ . 'ObjController.php';
+		/* important to check the PHP version */
+		// inport main classes
+		include_once _SQ_CLASSES_DIR_ . 'ObjController.php';
 
-        // Load helpers
-        SQ_Classes_ObjController::getClass('SQ_Classes_Helpers_Tools');
-        SQ_Classes_ObjController::getClass('SQ_Classes_Helpers_Sanitize');
-        // Load the Front and Block controller
-        SQ_Classes_ObjController::getClass('SQ_Classes_FrontController');
-        SQ_Classes_ObjController::getClass('SQ_Classes_BlockController');
+		// Load helpers
+		SQ_Classes_ObjController::getClass( 'SQ_Classes_Helpers_Tools' );
+		SQ_Classes_ObjController::getClass( 'SQ_Classes_Helpers_Sanitize' );
+		// Load the Front and Block controller
+		SQ_Classes_ObjController::getClass( 'SQ_Classes_FrontController' );
+		SQ_Classes_ObjController::getClass( 'SQ_Classes_BlockController' );
 
-        // Upgrade Squirrly call.
-        register_activation_hook(__FILE__, array(SQ_Classes_ObjController::getClass('SQ_Classes_Helpers_Tools'), 'sq_activate'));
-        register_deactivation_hook(__FILE__, array(SQ_Classes_ObjController::getClass('SQ_Classes_Helpers_Tools'), 'sq_deactivate'));
+		// Upgrade Squirrly call.
+		register_activation_hook( __FILE__, array(
+			SQ_Classes_ObjController::getClass( 'SQ_Classes_Helpers_Tools' ),
+			'sq_activate'
+		) );
+		register_deactivation_hook( __FILE__, array(
+			SQ_Classes_ObjController::getClass( 'SQ_Classes_Helpers_Tools' ),
+			'sq_deactivate'
+		) );
 
-        if (SQ_Classes_Helpers_Tools::isBackedAdmin()) {
-            SQ_Classes_ObjController::getClass('SQ_Classes_FrontController')->runAdmin();
-        } else {
-            SQ_Classes_ObjController::getClass('SQ_Classes_FrontController')->runFrontend();
-        }
+		if ( SQ_Classes_Helpers_Tools::isBackedAdmin() ) {
+			SQ_Classes_ObjController::getClass( 'SQ_Classes_FrontController' )->runAdmin();
+		} else {
+			SQ_Classes_ObjController::getClass( 'SQ_Classes_FrontController' )->runFrontend();
+		}
 
 
-    } catch (Exception $e) {
-    }
+	} catch ( Exception $e ) {
+	}
 }
